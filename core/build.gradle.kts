1ad013e4865c8245cc8f8fbfcb24053426ff48b9
earamalkhala3-cmyk <earam.alkhala3@gmail.com>
2026-09-16 02:55:54 +0300

    Fix core Gradle repository configuration

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,8 +2,6 @@ plugins { kotlin("jvm") }
 
 kotlin { jvmToolchain(17) }
 
-repositories { mavenCentral() }
-
 dependencies { testImplementation(kotlin("test")) }
 
 tasks.test { useJUnitPlatform() }
