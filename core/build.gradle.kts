f7a67112dd785d10f63715680d3f710008971ffa
earamalkhala3-cmyk <earam.alkhala3@gmail.com>
2026-09-16 02:41:36 +0300

    Add EARAM Tabs core module

---
+++ core/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins { kotlin("jvm") }
+
+kotlin { jvmToolchain(17) }
+
+repositories { mavenCentral() }
+
+dependencies { testImplementation(kotlin("test")) }
+
+tasks.test { useJUnitPlatform() }
