7a71c9343dd9330673def267a44383625ebe10fe
earamalkhala3-cmyk <earam.alkhala3@gmail.com>
2026-09-18 23:50:49 +0300

    Place CMake arguments in defaultConfig native build DSL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,12 +3,12 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android { namespace = "com.earam.tabs"; compileSdk = 35
     defaultConfig { applicationId = "com.earam.tabs"; minSdk = 23; targetSdk = 35; versionCode = 2; versionName = "0.1.1"; testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk { abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86", "x86_64") }
+        externalNativeBuild { cmake { arguments("-DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON") } }
     }
     externalNativeBuild {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
             version = "3.22.1"
-            arguments("-DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON")
         }
     }
     ndkVersion = "27.2.12479018"
