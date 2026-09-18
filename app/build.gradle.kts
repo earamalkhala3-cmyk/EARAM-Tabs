5243889fb0617201b7aaed5826ebd1f0dfeb8011
earamalkhala3-cmyk <earam.alkhala3@gmail.com>
2026-09-18 04:51:54 +0300

    Enable uncompressed 16 KB native library packaging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ android { namespace = "com.earam.tabs"; compileSdk = 35
         cmake { path = file("src/main/cpp/CMakeLists.txt"); version = "3.22.1" }
     }
     ndkVersion = "27.2.12479018"
+    packaging { jniLibs { useLegacyPackaging = false } }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
     kotlinOptions { jvmTarget = "17" }
 }
